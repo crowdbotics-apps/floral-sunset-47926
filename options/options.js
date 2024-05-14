27ca062fd955459a5418aa24333f36956a851cbc
Crowdbotics <team@crowdbotics.com>
2024-05-14 18:11:11 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "floral-sunset-47926",
+  url: "https://floral-sunset-47926.botics.co",
+  api: "https://floral-sunset-47926.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
